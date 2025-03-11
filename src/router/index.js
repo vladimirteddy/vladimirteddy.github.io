45193673f8227ff3c965a47ecf2013916cd493b2
Công Tone Book <never.fail0695@gmail.com>
2025-03-11 15:12:10 +0700

    feat: update 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import FamilyHeritage from "../components/FamilyHeritage.vue";
 import ActivitiesPage from "../views/ActivitiesPage.vue";
 import ActivityDetail from "../views/ActivityDetail.vue";
@@ -22,7 +22,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 });
 
